0cead5a59e310f3716a0a4817d5343c205ef0e16
Murillo Pinheiro de Oliveira <murillo.oliveira@sisfrete.com.br>
2026-07-03 11:47:25 -0300

    refactor: helper added in test

--- hello_world_test.go
+++ hello_world_test.go
@@ -10,22 +10,26 @@ func BenchmarkRandInt(b *testing.B) {
 		rand.Int()
 	}
 }
+
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Murillo")
 		want := "Hello, Murillo"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying hello with empty string", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, world"
+		want := "Hello, worldd"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
