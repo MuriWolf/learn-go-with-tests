71246c909c4bda28190b580200f48137658bb1ac
Murillo Pinheiro de Oliveira <murillo.oliveira@sisfrete.com.br>
2026-07-06 10:40:58 -0300

    feat: Benchmark and example added, repeat function improved

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,17 +1,42 @@
 package iteration
 
-import "strings"
+import (
+	"strings"
+)
 
-func Repeat(text string, iterations int) string {
+const (
+	Str    = "string"
+	Wrt    = "write"
+	WrtStr = "writeString"
+)
+
+func Repeat(text string, iterations int, method string) string {
 	if iterations < 0 {
 		iterations = 0
 	}
+	var repeated string
 
-	var repeated strings.Builder
-	for range iterations {
-		repeated.Write([]byte(text))
+	if method == Wrt {
+		var repeatedRaw strings.Builder
+		for range iterations {
+			repeatedRaw.Write([]byte(text))
+		}
+		repeated = repeatedRaw.String()
+	} else if method == WrtStr {
+		var repeatedRaw strings.Builder
+		for range iterations {
+			repeatedRaw.WriteString(text)
+		}
+		repeated = repeatedRaw.String()
+
+	} else if method == Str {
+		for range iterations {
+			repeated += text
+		}
+	} else {
+		return ""
 	}
-	return repeated.String()
+	return repeated
 }
 
 // TODO: Implement WriteString and string (for benchmark purposes)
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,14 +1,51 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", 5)
-	expected := "aaaaa"
+	t.Run("repeat with string", func(t *testing.T) {
+		repeated := Repeat("a", 5, "string")
+		expected := "aaaaa"
+		assertIsExpected(t, repeated, expected)
+	})
 
+	t.Run("repeat with write", func(t *testing.T) {
+		repeated := Repeat("a", 5, "write")
+		expected := "aaaaa"
+		assertIsExpected(t, repeated, expected)
+	})
+
+	t.Run("repeat with write string", func(t *testing.T) {
+		repeated := Repeat("a", 5, "writeString")
+		expected := "aaaaa"
+		assertIsExpected(t, repeated, expected)
+	})
+
+	t.Run("repeat with undefined method", func(t *testing.T) {
+		repeated := Repeat("a", 5, "notDefined")
+		expected := ""
+		assertIsExpected(t, repeated, expected)
+
+	})
+}
+
+func assertIsExpected(t *testing.T, repeated, expected string) {
 	if repeated != expected {
 		t.Errorf("got %q but expected %q", repeated, expected)
 	}
 }
 
-// TODO:: Implement benchmark
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 5, "writeString")
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 3, "writeString")
+	fmt.Println(repeated)
+	// Output: aaa
+}
