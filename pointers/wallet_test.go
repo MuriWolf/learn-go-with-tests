f7753f14922591a4524a703e5d7df98712fb0eb4
Murillo Pinheiro de Oliveira <murillo.oliveira@sisfrete.com.br>
2026-07-14 11:44:14 -0300

    feat: Addition of withdraw function and assertWallet

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,18 +6,28 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10.50))
-
-		got := wallet.Balance()
-		want := Bitcoin(10.50)
-
+	assertWallet := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		fmt.Printf("the address of the wallet in test is: %p \n", &wallet.balance)
 
+		got := wallet.Balance()
 		if want != got {
 			t.Errorf("want %s got %s", want, got)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10.50))
+
+		assertWallet(t, wallet, Bitcoin(10.50))
+
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: 25}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertWallet(t, wallet, Bitcoin(15))
 	})
 }
